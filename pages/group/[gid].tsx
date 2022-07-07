38453a0a0f040d56ae390eb9a0ac37378ccd206f
Austin Lee <austin.dh.lee@gmail.com>
2022-07-06 21:41:01 -0700

    fix groups ssr

--- pages/group/[gid].tsx
+++ pages/group/[gid].tsx
@@ -6,24 +6,22 @@ import { isEmpty } from "ramda";
 import { Transaction } from "plaid";
 import { tempStore } from "@/utils/store";
 import Link from "next/link";
+import { verifyUser } from "@/utils/ssr";
+import { Main } from "@/components/Main";
+import { Navbar } from "@/components/Navbar";
+import { Button } from "@/components/Button";
+import { ArrowLeftIcon, MixerHorizontalIcon } from "@radix-ui/react-icons";
+import theme from "@/styles/theme";
 
-const Group = () => {
+const Group = ({ user, profile, transactions, users }) => {
 	const router = useRouter();
 	const { gid }: { gid: string } = router.query;
-
 	const [showAddTransactions, setShowAddTransactions] = useState(false);
-	const [groupUsers, setGroupUsers] = useState([]);
-	const sharedTransactions = tempStore((state) => state.sharedTransactions);
-	const setSharedTransactions = tempStore.getState().setSharedTransactions;
+	const [groupUsers, setGroupUsers] = useState(users);
+	const [sharedTransactions, setSharedTransactions] = useState(transactions);
 
 	useEffect(() => {
-		setSharedTransactions([]);
 		if (!gid) return;
-		supabase
-			.from("shared_transactions")
-			.select()
-			.eq("group_id", gid)
-			.then(({ data, error }) => setSharedTransactions(data));
 
 		// subscribe to shared_transactions table based on group_id
 		supabase
@@ -42,9 +40,6 @@ const Group = () => {
 				fetchGroupUsers();
 			})
 			.subscribe();
-
-		fetchGroupUsers();
-		return () => setSharedTransactions([]);
 	}, [gid]);
 
 	const fetchGroupUsers = async () => {
@@ -65,8 +60,19 @@ const Group = () => {
 	};
 
 	return (
-		<div>
-			<p>Group: {gid}</p>
+		<Main>
+			<Navbar />
+			<div>{groupUsers[0].groups.name}</div>
+			<div className={"flex justify-between"}>
+				<Button size={"sm"} style={{ background: theme.colors.gradient.a }}>
+					<ArrowLeftIcon />
+					Return
+				</Button>
+				<Button size={"sm"} style={{ background: theme.colors.gradient.a }}>
+					<MixerHorizontalIcon />
+					Manage
+				</Button>
+			</div>
 			<div>
 				{groupUsers.map((user) => (
 					<div key={user.profile_id}>{user.profiles.username}</div>
@@ -87,8 +93,28 @@ const Group = () => {
 
 			<button onClick={addTransactions}>Add transactions</button>
 			{showAddTransactions && <AddTransactions gid={gid} sharedTransactions={sharedTransactions} />}
-		</div>
+		</Main>
 	);
 };
+
+export async function getServerSideProps({ req }) {
+	const { props, redirect } = await verifyUser(req);
+
+	const gidRegEx = new RegExp("(?<=gid=).*");
+	const result = gidRegEx.exec(req.url);
+
+	const gid = result ? result[0] : req.url.toString().slice(7);
+	const { data: transactions } = await supabase
+		.from("shared_transactions")
+		.select()
+		.eq("group_id", gid);
+
+	const { data: users } = await supabase
+		.from("profiles_groups")
+		.select("profile_id, profiles(username), groups(name)")
+		.eq("group_id", gid);
+
+	return { props: { ...props, transactions, users }, redirect };
+}
 
 export default Group;
