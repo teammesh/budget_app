ba64e918a92c333094c6c218eada62f7eff21c50
Austin Lee <austin.dh.lee@gmail.com>
2022-07-10 17:06:06 -0700

    fix plaid save pm

--- pages/api/plaidExchangeToken.ts
+++ pages/api/plaidExchangeToken.ts
@@ -1,11 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "@supabase/supabase-js";
 const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
-// @ts-ignore
-const supabaseService = createClient(supabaseUrl, supabaseServiceKey);
 
 const configuration = new Configuration({
 	// @ts-ignore
@@ -23,7 +17,7 @@ const client = new PlaidApi(configuration);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	try {
-		const { public_token, profile_id } = JSON.parse(req.body);
+		const { public_token } = JSON.parse(req.body);
 
 		const response = await client.itemPublicTokenExchange({
 			public_token,
@@ -31,19 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		const accessToken = response.data.access_token;
 		const itemID = response.data.item_id;
 
-		// use balance endpoint to get additional pm data
-		const balance = await client.accountsBalanceGet({ access_token: accessToken });
-
-		const { data, error } = await supabaseService.from("plaid_items").insert({
-			profile_id,
-			access_token: accessToken,
-			item_id: itemID,
-			account_id: balance.data.accounts[0].account_id,
-			last_four_digits: balance.data.accounts[0].mask,
-			name: balance.data.accounts[0].name,
-		});
-
-		res.status(200).json({ data, error });
+		res.status(200).json({ item_id: itemID, access_token: accessToken });
 	} catch (error) {
 		res.status(401).json({ error });
 	}
---
+++ pages/api/plaidSavePaymentMethod.ts
@@ -0,0 +1,44 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@supabase/supabase-js";
+const { Configuration, PlaidApi, PlaidEnvironments } = require("plaid");
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
+// @ts-ignore
+const supabaseService = createClient(supabaseUrl, supabaseServiceKey);
+
+const configuration = new Configuration({
+	// @ts-ignore
+	basePath: PlaidEnvironments[process.env.PLAID_ENV],
+	baseOptions: {
+		headers: {
+			"PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+			"PLAID-SECRET": process.env.PLAID_SECRET,
+			"Plaid-Version": "2020-09-14",
+		},
+	},
+});
+
+const client = new PlaidApi(configuration);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const { access_token, item_id, profile_id } = JSON.parse(req.body);
+
+		// use balance endpoint to get additional pm data
+		const balance = await client.accountsBalanceGet({ access_token });
+
+		const { data, error } = await supabaseService.from("plaid_items").insert({
+			profile_id,
+			access_token,
+			item_id,
+			account_id: balance.data.accounts[0].account_id,
+			last_four_digits: balance.data.accounts[0].mask,
+			name: balance.data.accounts[0].name,
+		});
+
+		res.status(200).json({ data, error });
+	} catch (error) {
+		res.status(401).json({ error });
+	}
+}
