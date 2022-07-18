4861e1fb8603a89b89bddb49c05e7f0acbd7456d
Austin Lee <austin.dh.lee@gmail.com>
2022-07-17 21:08:30 -0700

    update activity icon

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -1,4 +1,9 @@
-import { ActivityEditedIcon, ActivityPaidIcon, ActivitySharedIcon } from "@/components/icons";
+import {
+	ActivityEditedIcon,
+	ActivityPaidIcon,
+	ActivityRemovedIcon,
+	ActivitySharedIcon,
+} from "@/components/icons";
 import { PrimaryBox } from "@/components/boxes";
 
 export const Activity = ({ activity }: { activity: any }) => {
@@ -20,36 +25,58 @@ export const Activity = ({ activity }: { activity: any }) => {
 		[tableNames.transactions]: {
 			[activityType.insert]: <ActivitySharedIcon />,
 			[activityType.update]: <ActivityEditedIcon />,
-			[activityType.delete]: <ActivityEditedIcon />,
+			[activityType.delete]: <ActivityRemovedIcon />,
 		},
 		[tableNames.payments]: {
 			[activityType.insert]: <ActivityPaidIcon />,
-			[activityType.update]: <ActivityPaidIcon />,
-			[activityType.delete]: <ActivityPaidIcon />,	
+			[activityType.update]: <ActivityEditedIcon />,
+			[activityType.delete]: <ActivityRemovedIcon />,
 		},
 	};
 
 	const descriptionMap = {
 		[tableNames.transactions]: {
-			[activityType.insert]: <><b>{username}</b> shared a new transaction </>,
-			[activityType.update]: <><b>{username}</b> edited a transaction</>,
-			[activityType.delete]: <><b>{username}</b> deleted a transaction</>,
+			[activityType.insert]: (
+				<>
+					<b>{username}</b> shared a new transaction{" "}
+				</>
+			),
+			[activityType.update]: (
+				<>
+					<b>{username}</b> edited a transaction
+				</>
+			),
+			[activityType.delete]: (
+				<>
+					<b>{username}</b> deleted a transaction
+				</>
+			),
 		},
 		[tableNames.payments]: {
-			[activityType.insert]: <><b>{username}</b> paid <b>{toUsername}</b></>,
-			[activityType.update]: <><b>{username}</b> updated payment to <b>{toUsername}</b></>,
-			[activityType.delete]: <><b>{username}</b> deleted payment to <b>{toUsername}</b></>,	
+			[activityType.insert]: (
+				<>
+					<b>{username}</b> paid <b>{toUsername}</b>
+				</>
+			),
+			[activityType.update]: (
+				<>
+					<b>{username}</b> updated payment to <b>{toUsername}</b>
+				</>
+			),
+			[activityType.delete]: (
+				<>
+					<b>{username}</b> deleted payment to <b>{toUsername}</b>
+				</>
+			),
 		},
 	};
 
-	return (	
+	return (
 		<PrimaryBox key={activity.id}>
 			<div className={"grid grid-cols-[auto_1fr_auto] gap-4"}>
 				{iconMap[activity.table_name][activity.type]}
 				<div className={"grid grid-cols-1"}>
-					<p className={"text-sm"}>
-						{descriptionMap[activity.table_name][activity.type]}
-					</p>
+					<p className={"text-sm"}>{descriptionMap[activity.table_name][activity.type]}</p>
 					<p className={"text-sm text-gray-600 font-mono tracking-tighter"}>4 hours ago</p>
 				</div>
 			</div>
