105e880bf4e1337b8c8fa5a8af29a5e341c035fd
erichuiwong <cpterack@gmail.com>
2022-07-17 21:00:44 -0700

    use maps for activity

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -1,55 +1,54 @@
 import { ActivityEditedIcon, ActivityPaidIcon, ActivitySharedIcon } from "@/components/icons";
 import { PrimaryBox } from "@/components/boxes";
-import { tempStore } from "@/utils/store";
-import { isEmpty } from "ramda";
 
-export const Activity = ({activity}: {activity: any}) => {
-	const activities = tempStore((state) => state.groupActivities);
+export const Activity = ({ activity }: { activity: any }) => {
+	const username = activity.user.username;
+	const toUsername = activity.to_user?.username;
 
 	const tableNames = {
 		transactions: "shared_transactions",
 		payments: "payments",
 	};
 
-	const icon = (type: string) => {
-		switch(type) {
-			case "INSERT": 
-				return <ActivitySharedIcon />;
-			case "UPDATE":
-				return <ActivityEditedIcon />;
-			case "DELETE":
-				return <ActivityEditedIcon />;
-		}
+	const activityType = {
+		insert: "INSERT",
+		update: "UPDATE",
+		delete: "DELETE",
 	};
 
-	const description = (profile: any, table_name: string, type: string, to_profile?: any) => {
-		const username = profile.username;
-		if (table_name === tableNames.transactions) {
-			if (type === "INSERT") {
-				return <><b>{username}</b> shared a new transaction </>;
-			} else if (type === "UPDATE") {
-				return <><b>{username}</b> edited a transaction</>;
-			} else {
-				return <><b>{username}</b> deleted a transaction</>;
-			}
-		} else if (table_name === tableNames.payments) {
-			if (type === "INSERT") {
-				return <><b>{username}</b> paid <b>{to_profile.username}</b></>;
-			} else if (type === "UPDATE") {
-				return <><b>{username}</b> updated payment to <b>{to_profile.username}</b></>;
-			} else {
-				return <><b>{username}</b> deleted payment to <b>{to_profile.username}</b></>;
-			}
-		}
+	const iconMap = {
+		[tableNames.transactions]: {
+			[activityType.insert]: <ActivitySharedIcon />,
+			[activityType.update]: <ActivityEditedIcon />,
+			[activityType.delete]: <ActivityEditedIcon />,
+		},
+		[tableNames.payments]: {
+			[activityType.insert]: <ActivityPaidIcon />,
+			[activityType.update]: <ActivityPaidIcon />,
+			[activityType.delete]: <ActivityPaidIcon />,	
+		},
+	};
+
+	const descriptionMap = {
+		[tableNames.transactions]: {
+			[activityType.insert]: <><b>{username}</b> shared a new transaction </>,
+			[activityType.update]: <><b>{username}</b> edited a transaction</>,
+			[activityType.delete]: <><b>{username}</b> deleted a transaction</>,
+		},
+		[tableNames.payments]: {
+			[activityType.insert]: <><b>{username}</b> paid <b>{toUsername}</b></>,
+			[activityType.update]: <><b>{username}</b> updated payment to <b>{toUsername}</b></>,
+			[activityType.delete]: <><b>{username}</b> deleted payment to <b>{toUsername}</b></>,	
+		},
 	};
 
 	return (	
 		<PrimaryBox key={activity.id}>
 			<div className={"grid grid-cols-[auto_1fr_auto] gap-4"}>
-				{icon(activity.type)}
+				{iconMap[activity.table_name][activity.type]}
 				<div className={"grid grid-cols-1"}>
 					<p className={"text-sm"}>
-						{description(activity.user, activity.table_name, activity.type, activity.to_user)}
+						{descriptionMap[activity.table_name][activity.type]}
 					</p>
 					<p className={"text-sm text-gray-600 font-mono tracking-tighter"}>4 hours ago</p>
 				</div>
