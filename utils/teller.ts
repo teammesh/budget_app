acb23e8348167bcd317142a200d3f6ec9b2da7ad
Austin Lee <austin.lee@calcivilrights.ca.gov>
2025-01-11 18:54:46 -0800

    fix file path

--- utils/teller.ts
+++ utils/teller.ts
@@ -1,13 +1,17 @@
 import { Agent } from "undici";
+import path from "path";
 import fs from "fs";
+
+const certPath = path.join(process.cwd(), process.env.TELLER_CLIENT_CERT_PATH!);
+const keyPath = path.join(process.cwd(), process.env.TELLER_CLIENT_KEY_PATH!);
 
 export const tellerApiRequest = async (endpoint: string, options: any) => {
 	const response =await fetch(`https://api.teller.io/${endpoint}`, {
 		// @ts-ignore
 		dispatcher: new Agent({
 			connect: {
-				cert: fs.readFileSync(process.env.TELLER_CLIENT_CERT_PATH!),
-				key: fs.readFileSync(process.env.TELLER_CLIENT_KEY_PATH!),
+				cert: fs.readFileSync(certPath),
+				key: fs.readFileSync(keyPath),
 			},
 		}),
 		method: options.method,
